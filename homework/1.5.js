89724e15ad7742fbef9600ec39fbe058d8063715
ththht888 <o.morozova2016@gmail.com>
2024-11-21 21:47:29 +0500

    popravki

--- homework/1.5.js
+++ homework/1.5.js
@@ -2,19 +2,13 @@
 // Найдите сумму всех целых чисел от 1 до 100.
 // let sum = 0;
 
-// for (let i = 1; i <= 100; i++) {
-//     sum += i;
-// }
-
-// console.log(sum);
+// for (let i = 1; i <= 100; i++) sum += i console.log(sum);
 
 // 2
 // let sum = 0;
 
 // for (let i = 1; i <= 100; i++) {
-//   if (i % 2 === 0) {
-//     sum += i;
-//   }
+//   if (i % 2 === 0) sum += i;
 // }
 // console.log(sum);
 
@@ -22,24 +16,21 @@
 // Найдите сумму всех целых нечетных чисел в промежутке от 1 до 100.
 // let sum = 0
 // for (let i = 1; i <= 100; i++) {
-//     if (i % 2 !== 0) {
-//         sum += i
-//     }
+//     if (i % 2 !== 0) sum += i
 // }
 // console.log(sum)
 
 // 4
 // Даны два целых числа. Найдите остаток от деления первого числа на второе.
-// const num1 = 2345
-// const num2 = 9876
+// const num1 = 5
+// const num2 = 3
+// console.log(num1 % num2)
 
 // 5
 // Дана некоторая строка. Переберите и выведите в консоль по очереди все символы с конца строки.
 
 // function stringEnd(string) {
-//   for (let i = string.length - 1; i >= 0; i--) {
-//     console.log(string[i]);
-//   }
+//   for (let i = string.length - 1; i >= 0; i--) console.log(string[i]);
 // }
 // const str = "fiftycent";
 // stringEnd(str);
