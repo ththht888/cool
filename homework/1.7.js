c364eb2271710e2d67d515ffe06615cf4624e669
ththht888 <o.morozova2016@gmail.com>
2024-11-25 20:03:56 +0500

    1.7

--- homework/1.7.js
+++ homework/1.7.js
@@ -1,6 +1,42 @@
 // 1
-const str = 'abcde'
-console.log(str.split(''))
+// Дана строка:
+// 'abcde'
+// Получите массив букв этой строки.
+
+// const str = 'abcde'
+// console.log(str.split(''))
 
 // 2
+// Дано некоторое число:
+// 12345
+// Получите массив цифр этого числа.
+
+// const num = 12345
+// const str = String(num)
+// const arr = []
+// for (let i = 0; i < str.length; i++)  arr.push(+str[i])
+// console.log(arr)
+
+// 3
+// Дано некоторое число:
+// 12345
+// Переверните его:
+// 54321
+
+// const num = 12345;
+// const str = String(num);
+// const arr = [];
+// for (let i = str.length - 1; i >= 0; i--) arr.push(+str[i]);
+// console.log(arr);
+
+// 4
+// Дано некоторое число:
+// 12345
+// Найдите сумму цифр этого числа.
+
 const num = 12345
+// const str = String(num)
+// const arr = []
+// let sum = 0
+// for (let i = 0; i < str.length; i++) sum += i; arr.push(+str[i])
+// console.log(arr)
---
+++ homework/1.9.js
@@ -0,0 +1,17 @@
+// 1
+// Дан массив со строками. Оставьте в этом массиве только те строки, которые начинаются на http
+// const str = ['http://js.com', 'https://css.com', 'http://html.com']
+// const str2 = str.filter(str => str.startsWith("http://"));
+// console.log(str2);
+
+// 2
+// Дан массив со строками. Оставьте в этом массиве только те строки, которые заканчиваются на .html.
+// const str = ['http://js.com', 'https://css.com', 'http://html.html']
+// const str2 = str.filter(str => str.endsWith(".html"));
+// console.log(str2);
+
+// 3
+// Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.
+// const arr = [10, 200, 60, 50]
+// const arr2 = arr.map(arr => arr * 1.1);
+// console.log(arr2)
